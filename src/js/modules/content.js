57f44926ee82b840dfd9730929b3dea2992a6cb3
Adam L <github@adamin.net>
2014-07-22 18:03:35 -0700

    Added tags map

--- src/js/modules/content.js
+++ src/js/modules/content.js
@@ -4,12 +4,23 @@ var $    = require('domtastic/bundle/full/domtastic')
 ,   _    = require('lodash')
 ,   Tags = require('./tag-names');
 
+
+var getTags = function() {
+  return _.map(Tags, function(e) {
+    return e.name;
+  })
+};
+
+var getNodes = function(tag) {
+  return $('.' + tag);
+};
+
 var content = {
 
-  init: {
-    _.forEach(Tags, function(k, v, c) {
-      console.log(k,v,c);
-    })
+  init: function() {
+    _.forEach(getTags(), function(e) {
+      console.log(getNodes(e));
+    });
   }
 
 };
