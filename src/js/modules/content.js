d96463ba5413fbe7fc6487215957f49a037c6d59
Adam L <github@adamin.net>
2014-07-22 17:18:29 -0700

    Added tagname info

--- src/js/modules/content.js
+++ src/js/modules/content.js
@@ -1,9 +1,16 @@
 'use strict';
 
-var $ = require('domtastic/bundle/full/domtastic');
+var $    = require('domtastic/bundle/full/domtastic')
+,   _    = require('lodash')
+,   Tags = require('./tag-names');
 
 var content = {
 
+  init: {
+    _.forEach(Tags, function(k, v, c) {
+      console.log(k,v,c);
+    })
+  }
 
 };
 
