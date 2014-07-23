5d4d37f474e3a5965f47558e6e4ba5ba659af938
Adam L <github@adamin.net>
2014-07-23 11:02:34 -0700

    Added li to box

--- src/js/modules/obj.js
+++ src/js/modules/obj.js
@@ -3,52 +3,68 @@
 var $           = require('domtastic/bundle/full/domtastic')
 ,   _           = require('lodash')
 ,   objTemplate = require('./template')
-,   getNodes    = require('./content');
+,   getNodes    = require('./content')
+,   Tags        = require('./tag-names');
 
-var body   = document.getElementsByTagName('body')[ 0 ]
-,   head   = document.getElementsByTagName('head')[ 0 ]
-,   cssLoc = 'https://seethroughtrees.github.io/smartling-tags-checker/index.css';
+var cssLoc = 'https://seethroughtrees.github.io/smartling-tags-checker/index.css';
 
 
-function append(div) {
-  body.appendChild(div);
+function addStyle() {
+  var $link = $('<li>');
+
+  $link.attr({
+    rel: 'stylesheet',
+    href: cssLoc,
+    type: 'text/css'
+  })
+
+  $('head').append($link);
 }
 
 
-function style(div) {
-  var link = document.createElement('link');
-  link.setAttribute('rel','stylesheet');
-  link.setAttribute('href', cssLoc);
-  link.setAttribute('type','text/css');
-  head.appendChild(link);
-  append(div);
+function getTagLink(el) {
+  var tagType = $(el).attr('data-stc');
+  return _.find(Tags, {name: tagType}).link;
 }
+
+function addContainer() {
+  var $container = $('<ul>');
+
+  $container.attr('id', 'smartling-obj');
+
+  _.forEach(getNodes(), function(el, ind, arr) {
+    var $li     = $('<li>'),
+        $a      = $('<a>'),
+        $link   = $('<a>'),
+        $body   = $('body'),
+        tagType = $(el).attr('data-stc');
+
+    $a.attr({
+        id: tagType,
+        href: '#'
+      })
+      .addClass('is-active')
+      .text(tagType);
+
+    $link.attr({
+        href: getTagLink(el)
+      })
+      .addClass('icon-link')
+      .text(' - link');
+
+    $li.append($a)
+      .append($link);
+
+    $container.append($li)
+    $body.append($container);
+
+  });
+};
 
 
 function init() {
-  console.log(getNodes());
-  var $container = $('<ul>');
-  $container.attr('id', 'smartling-obj');
-
-  // _.forEach(getNodes(), function(el, ind, arr) {
-  //   var $li = $(li);
-  //   $li.text()
-
-  //   $container.append($li)
-
-  // });
-
-  $('body').append($container);
-
-  // var div = document.createElement('div');
-  // div.setAttribute('id', 'smart-obj');
-  // div.innerHTML = objTemplate;
-  // style(div);
-
-  // _.forEach(getNodes, function(e, i, v) {
-  //   $('#smartling-obj')
-  // })
-
+  addContainer();
+  addStyle();
 }
 
 
