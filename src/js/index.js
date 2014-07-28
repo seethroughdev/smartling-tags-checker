5b5e7a55cd1158b0c1ba342dfa5ef8a6df244369
Adam L <github@adamin.net>
2014-07-27 21:23:19 -0700

    Update 2014-07-28T04:23:19.863Z

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-var events  = require('./modules/events')
-,   obj     = require('./modules/obj')
-,   tags    = require('./modules/tag-names')
-,   content = require('./modules/content');
-
-obj(content(tags), tags);
-events();
--- src/js/modules/template.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var objTemplate = '<span id="stc-heading">Smartling Tags</span>';
-
-module.exports = objTemplate;
